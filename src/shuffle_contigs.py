29e22aa8d06eea80be358a5db5a2155d0ff7710b
SebastianDall <semoda@bio.aau.dk>
2024-11-20 13:04:31 +0100

    new way of constructing methylation contamination where all bins gets a contaminant

--- src/shuffle_contigs.py
+++ src/shuffle_contigs.py
@@ -1,4 +1,3 @@
-import os
 import polars as pl
 import pandas as pd
 import random
@@ -16,41 +15,77 @@ def associate_bin_with_contigs(contig_bin):
     
 
 
-def shuffle_contigs_between_bins(bin_contigs, num_contigs_to_shuffle):
-    contigs_to_move = {}
-
-    for bin in bin_contigs.keys():
-        contigs = bin_contigs[bin]
-        # INFO: Since we are no longer removing contigs. Bigger contigs can also be copied to other bins.
-        # if len(contigs) <= num_contigs_to_shuffle:
-        #     continue
-
-        print("Processing bin:", bin)
-        print("Processing contigs:", contigs)
-        contigs_for_shuffling = random.sample(contigs, num_contigs_to_shuffle)
-
-        contigs_to_move[bin] = contigs_for_shuffling
-
-        # INFO: Removing contigs at random can create some weird cases where the biggest contig in a bin is moved. This will mean a bin could be a very low quality bin that we are trying to assess.
-        # # remove contigs to be shuffled.
-        # bin_contigs[bin] = [c for c in contigs if c not in contigs_for_shuffling]
-
-    shuffled_items = list(contigs_to_move.items())
-    random.shuffle(shuffled_items)
+def shuffle_contigs_between_bins(contig_bin, contigs_to_shuffle, num_contigs_to_shuffle):
+    new_contig_bin = copy.deepcopy(contig_bin)
     
-    bin_wo_contaminants = list(bin_contigs.keys())
-    for source_bin, contigs in shuffled_items:
-        for contig in contigs:
-            possible_bins_for_assignment = [b for b in contigs_to_move.keys() if b != source_bin and "coli" not in b and b in bin_wo_contaminants]
-            if len(possible_bins_for_assignment) == 0:
-                print(f"No possible bin for assigning {contig} from {source_bin}")
-                break
-            
-            new_bin = random.choice(possible_bins_for_assignment)
-            bin_contigs[new_bin].append("contamination_" + contig)
-            bin_wo_contaminants.remove(new_bin)
+    bins_wo_contaminants = list(contig_bin.keys())
+    bins_wo_contaminants = [b for b in bins_wo_contaminants if "coli" not in b]
 
-    return bin_contigs
+    bins_where_contaminants_have_been_taken = set()
+    contigs_used_as_contaminants = set()
+    
+    while bins_wo_contaminants:
+        print("Remaining bins without contaminants: ", len(bins_wo_contaminants))
+        print(bins_wo_contaminants)
+
+        bins_availble_for_taking_contamination = [b for b in contigs_to_shuffle.keys() if b not in bins_where_contaminants_have_been_taken]
+        if not bins_availble_for_taking_contamination:
+            bins_availble_for_taking_contamination = list(bins_where_contaminants_have_been_taken)
+
+        bin_to_take_from = random.choice(bins_availble_for_taking_contamination)
+        contigs_available = contigs_to_shuffle[bin_to_take_from]
+        contigs_available = [c for c in contigs_available if c not in contigs_used_as_contaminants]
+        if not contigs_available:
+            bins_where_contaminants_have_been_taken.add(bin_to_take_from)
+            continue
+
+        contig_selected = random.choice(contigs_available)
+        bin_to_assign_to = random.choice(bins_wo_contaminants)
+
+        if not contig_selected in contig_bin[bin_to_assign_to]:
+            new_contig_bin[bin_to_assign_to].append("contamination_" + contig_selected)
+            bins_wo_contaminants.remove(bin_to_assign_to)
+            contigs_used_as_contaminants.add(contig_selected)
+            bins_where_contaminants_have_been_taken.add(bin_to_take_from)
+
+    return new_contig_bin
+
+        
+
+        
+    
+    # for bin in bin_contigs.keys():
+    #     contigs = bin_contigs[bin]
+    #     # WARNING: moving big contigs can take over the methylation pattern, which will destroy the bin.
+    #     if len(contigs) <= num_contigs_to_shuffle:
+    #         continue
+
+    #     print("Processing bin:", bin)
+    #     print("Processing contigs:", contigs)
+    #     contigs_for_shuffling = random.sample(contigs, num_contigs_to_shuffle)
+
+    #     contigs_to_move[bin] = contigs_for_shuffling
+
+    #     # INFO: Removing contigs at random can create some weird cases where the biggest contig in a bin is moved. This will mean a bin could be a very low quality bin that we are trying to assess.
+    #     # # remove contigs to be shuffled.
+    #     # bin_contigs[bin] = [c for c in contigs if c not in contigs_for_shuffling]
+
+    # shuffled_items = list(contigs_to_move.items())
+    # random.shuffle(shuffled_items)
+    
+    # bin_wo_contaminants = list(bin_contigs.keys())
+    # for source_bin, contigs in shuffled_items:
+    #     for contig in contigs:
+    #         possible_bins_for_assignment = [b for b in contigs_to_move.keys() if b != source_bin and "coli" not in b and b in bin_wo_contaminants]
+    #         if len(possible_bins_for_assignment) == 0:
+    #             print(f"No possible bin for assigning {contig} from {source_bin}")
+    #             break
+            
+    #         new_bin = random.choice(possible_bins_for_assignment)
+    #         bin_contigs[new_bin].append("contamination_" + contig)
+    #         bin_wo_contaminants.remove(new_bin)
+
+    # return bin_contigs
 
 # def create_pileup(pileup, contaminated_bin_contigs):
     
@@ -63,7 +98,7 @@ def remove_contigs_from_bin(bin_contigs, num_contigs_to_remove):
         if len(contigs) <= num_contigs_to_remove:
             continue
 
-        contigs_for_removal = random.sample(contigs, num_contigs_to_remove)
+        contigs_for_removal = random.choice(contigs)
 
         contigs_to_remove[bin] = contigs_for_removal
 
@@ -82,23 +117,46 @@ def write_contig_bin_file(bin_contigs, filename):
 
 if __name__ == '__main__':
     random.seed(42)
-    contig_bin = pd.read_csv("data/datasets/simulated_4_lognormal_20-100x/contig_mapping/mapped_contig_bin.tsv", sep = "\t", names=["contig", "bin"])
-
+    contig_bin = pl.read_csv("data/datasets/simulated_4_lognormal_20-100x/contig_mapping/mapped_contig_bin.tsv", 
+                             separator = "\t", has_header=False, new_columns=["contig", "bin"])
     contig_methylation = pl.read_csv("data/datasets/simulated_4_lognormal_20-100x/motifs-scored-read-methylation.tsv", separator = "\t")
 
     contigs_w_methylation = contig_methylation\
-        .filter(pl.col("N_motif_obs") > 8)\
-        .get_column("contig").unique()
+        .filter(pl.col("N_motif_obs") > 8)
 
-    contig_bin = contig_bin[contig_bin["contig"].isin(contigs_w_methylation)]
-    contigs_in_bin = associate_bin_with_contigs(contig_bin)
+    bin_observations = contigs_w_methylation.join(contig_bin, how = "left", on = ["contig"])\
+        .filter((pl.col("motif") == "GATC") & (pl.col("mod_type") == "a"))\
+        .group_by(["bin", "motif"])\
+        .agg(
+            pl.col("N_motif_obs").sum().alias("N_bin_motif_obs")
+        )
+        
+    smaller_contigs_w_methylation = contigs_w_methylation\
+        .filter((pl.col("motif") == "GATC") & (pl.col("mod_type") == "a"))\
+        .join(contig_bin, how = "left", on = ["contig"])\
+        .join(bin_observations, how="left", on = ["bin"])\
+        .with_columns(
+            obs_percentage = pl.col("N_motif_obs") / pl.col("N_bin_motif_obs")
+        )
 
+    print(smaller_contigs_w_methylation.filter(pl.col("contig").is_in(["contig_329", "contig_63"])))
+        
+    smaller_contigs_w_methylation = smaller_contigs_w_methylation.filter(pl.col("obs_percentage") < 0.5).get_column("contig")
+        
+
+    contig_bin = contig_bin.to_pandas()
+    contigs_to_shuffle = contig_bin[contig_bin["contig"].isin(smaller_contigs_w_methylation)]
+    contigs_to_shuffle = associate_bin_with_contigs(contigs_to_shuffle)
+
+    contig_bin_dict = associate_bin_with_contigs(contig_bin)
+
+    contigs_to_remove = contig_bin[contig_bin["contig"].isin(contigs_w_methylation.get_column("contig"))]
 
     for i in range(0,10):
         print(f"Shuffling benchmark {i}")
-        shuffled_contigs = shuffle_contigs_between_bins(copy.deepcopy(contigs_in_bin), 1)
+        shuffled_contigs = shuffle_contigs_between_bins(copy.deepcopy(contig_bin_dict), copy.deepcopy(contigs_to_shuffle), 1)
         write_contig_bin_file(shuffled_contigs, f"files/benchmarks/simulated_4_lognormal_20-100x/benchmark_{i}_shuffle_1_contig_w_signal.tsv")
         
-        shuffled_contigs = remove_contigs_from_bin(copy.deepcopy(contigs_in_bin), 1)
-        write_contig_bin_file(shuffled_contigs, f"files/benchmarks/simulated_4_lognormal_20-100x/benchmark_{i}_remove_1_contig_w_signal.tsv")
+        # shuffled_contigs = remove_contigs_from_bin(copy.deepcopy(contigs_to_remove), 1)
+        # write_contig_bin_file(shuffled_contigs, f"files/benchmarks/simulated_4_lognormal_20-100x/benchmark_{i}_remove_1_contig_w_signal.tsv")
         
